8a3a806f87a1a4a16e7e71a4c9fb1ce305bc37c0
Alex Curnow <alexcurnow@gmail.com>
2020-06-17 15:24:47 -0500

    Dogs are now listed in Index. New dogs can be created, their details can be viewed, and the user can edit and delete dogs.

---
+++ DogGo/Controllers/DogsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DogGo.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly DogRepository _dogRepo;
+        public DogsController(IConfiguration config)
+        {
+            _dogRepo = new DogRepository(config);
+        }
+        // GET: DogsController
+        public ActionResult Index()
+        {
+            List<Dog> dogs = _dogRepo.GetAllDogs();
+            return View(dogs);
+        }
+
+        // GET: DogsController/Details/5
+        public ActionResult Details(int id)
+        {
+            Dog dog = _dogRepo.GetDogById(id);
+            if (dog == null)
+            {
+                return NotFound();
+            }
+            return View(dog);
+        }
+
+        // GET: DogsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Dog dog)
+        {
+            try
+            {
+                _dogRepo.AddDog(dog);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(dog);
+            }
+        }
+
+        // GET: DogsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Dog dog = _dogRepo.GetDogById(id);
+            if (dog == null)
+            {
+                return NotFound();
+            }
+
+            return View(dog);
+        }
+
+        // POST: DogsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Dog dog)
+        {
+            try
+            {
+                _dogRepo.UpdateDog(dog);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(dog);
+            }
+        }
+
+        // GET: DogsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Dog dog = _dogRepo.GetDogById(id);
+            return View(dog);
+        }
+
+        // POST: DogsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Dog dog)
+        {
+            try
+            {
+                _dogRepo.DeleteDog(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(dog);
+            }
+        }
+    }
+}
